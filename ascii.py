6decb7a47165977acdd857edf15af8f8548bd88f
joeyuan19 <joe.yuan19@gmail.com>
2014-02-07 15:54:30 -0500

    readme

--- ascii.py
+++ ascii.py
@@ -2,11 +2,15 @@ from PIL import Image as I
 import numpy as np
 
 
-def asciify(img,width=80,invert=False):
+def asciify(img,width=80,invert=False,vert_scale=False):
     tmp = img.convert('L')
     X,Y = tmp.size
     factor = float(width)/X
-    tmp = tmp.resize((int(X*factor),int(Y*factor*.5)))
+    if vert_scale:
+        vert_scale = 0.5
+    else:
+        vert_scale = 1.0
+    tmp = tmp.resize((int(X*factor),int(Y*factor*vert_scale)))
     data = np.array(tmp)
     buf = ""
     for x in xrange(len(data)):
@@ -30,10 +34,11 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-w","--width",dest="width",default=80)
     parser.add_option("-i","--invert",action="store_true",dest="invert",default=False)
+    parser.add_option("-s","--no-vertical-scale",action="store_false",dest="vert_scale",default=True)
     opts,args = parser.parse_args()
     if len(args) <= 0:
         print "No image provided"
         sys.exit(1)
     img = I.open(args[0])
-    print asciify(img,opts.width,opts.invert)
+    print asciify(img,opts.width,opts.invert,opts.vert_scale)
 
